2651ecfa4e641db2bb660cac37100475e44dc600
IanFrancoI <166461314+IanFrancoI@users.noreply.github.com>
2025-06-04 20:21:12 -0300

    commit completado (para revisar y testear)

--- TP3/src/routes/turnos.routes.js
+++ TP3/src/routes/turnos.routes.js
@@ -5,9 +5,28 @@ const  {verifyTokenMiddleware}  = require('../middlewares/verifyToken.middleware
 const rutaTurnos = Router();
 
 rutaTurnos.get("/:idPaciente", turnosController.getTurnosByPaciente)
-rutaTurnos.delete("/:idTurno", verifyTokenMiddleware, turnosController.cancelTurno)
+rutaTurnos.delete("/:idTurno", turnosController.cancelTurno)
 rutaTurnos.post("/", verifyTokenMiddleware, turnosController.createTurno)
 rutaTurnos.get("/", turnosController.list)
 
 
 module.exports = rutaTurnos;
+
+const express = require('express');
+const router = express.Router();
+const turnosController = require('../controllers/API/turnos.controller');
+const AuthController = require('../controllers/API/auth.controller.js');
+const verifyToken = require('../middlewares/verifyToken.middleware.js');
+const TurnosController = require('../controllers/API/turnos.controller.js');
+
+const turnosController = new TurnosController();
+
+//login pai
+router.post('/v1/login', AuthController.login);
+
+//endpoints de los turnos
+router.get('/v1/turnos/pacientes/:id', turnosController.listByPaciente);
+router.post('/v1/turnos', verifyToken, turnosController.create);
+router.delete('/v1/turnos/:id', turnosController.delete);
+
+module.exports = router;
