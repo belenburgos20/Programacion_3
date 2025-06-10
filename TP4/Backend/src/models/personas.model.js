e36955a6e811e18a19468bce46e1db5a14148227
Luciano Guardese <lucho.guardese@gmail.com>
2025-06-09 22:03:41 -0300

    Inicio TP4

---
+++ TP4/Backend/src/models/personas.model.js
@@ -0,0 +1,39 @@
+const personas = [
+  {
+    id: 1,
+    nombre: "Luciano",
+    apellido: "Guardese",
+    edad: 30,
+    email: "Lucho.guardese@gmail.com"
+  },
+  {
+    id: 2,
+    nombre: "Ivan",
+    apellido: "Guardese",
+    edad: 25,
+    email: "Ivan.guardese@gmail.com"
+  },
+  {
+    id: 3,
+    nombre: "Noe",
+    apellido: "Hubert",
+    edad: 40,
+    email: "Noe.Hubert@gmail.com"
+  },
+  {
+    id: 4,
+    nombre: "Ian",
+    apellido: "Ibañez",
+    edad: 35,
+    email: "Ian.Ibañez@gmail.com"
+  },
+  {
+    id: 5,
+    nombre: "Belen",
+    apellido: "Burgos",
+    edad: 28,
+    email: "Belen.Burgos@gmail.com"
+  }
+];
+
+module.exports = personas;
