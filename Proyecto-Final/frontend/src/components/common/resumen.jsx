128a1fbfa7a960ef4ae1e9f0311a686bfaab2d75
belenburgos20 <belenburgos.dev@gmail.com>
2025-07-10 00:46:20 -0300

    Cambios en Resumen

--- Proyecto-Final/frontend/src/components/common/resumen.jsx
+++ Proyecto-Final/frontend/src/components/common/resumen.jsx
@@ -1,25 +1,36 @@
 //muestra ingresos totales, gastos y diferencia.
 
-import React from 'react';
+import React from "react";
 
 const Resumen = ({ ingresos, egresos }) => {
-  const totalIngresos = ingresos.reduce((acc, mov) => acc + mov.total, 0);
-  const gastosFijos = egresos 
-    .filter(mov => mov.categoria === 'fijo')
+  const totalIngresos = ingresos.reduce(
+    (acc, mov) => acc + parseFloat(mov.total),
+    0
+  );
+  const gastosFijos = egresos
+    .filter((mov) => mov.categoria === "fijo")
     .reduce((acc, mov) => acc + mov.total, 0);
   const gastosVariables = egresos
-    .filter(mov => mov.categoria === 'variable')
+    .filter((mov) => mov.categoria === "variable")
     .reduce((acc, mov) => acc + mov.total, 0);
   const diferencia = totalIngresos - gastosFijos - gastosVariables;
 
   return (
-    <div >
-      <h2 >Resumen</h2>
+    <div>
+      <h2>Resumen</h2>
       <ul>
-        <li>Ingresos netos: <span>${totalIngresos.toFixed(2)}</span></li>
-        <li>Gastos fijos: <span>${gastosFijos.toFixed(2)}</span></li>
-        <li>Gastos variables: <span>${gastosVariables.toFixed(2)}</span></li>
-        <li>Diferencia: <span>${diferencia.toFixed(2)}</span></li>
+        <li>
+          Total Ingresos: <span>${Number(totalIngresos).toFixed(2)}</span>
+        </li>
+        <li>
+          Gastos fijos: <span>${gastosFijos.toFixed(2)}</span>
+        </li>
+        <li>
+          Gastos variables: <span>${gastosVariables.toFixed(2)}</span>
+        </li>
+        <li>
+          Diferencia: <span>${diferencia.toFixed(2)}</span>
+        </li>
       </ul>
     </div>
   );
